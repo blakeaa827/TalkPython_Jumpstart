257a41c5e244eba6c54865bfe64afa565f6c2836
Blake <blakeaa@gmail.com>
2018-03-24 12:18:53 -0500

    App 1 complete

---
+++ 01_hello_world/program.py
@@ -0,0 +1,6 @@
+print('-----------------------------------')
+print('              HELLO')
+print('-----------------------------------')
+
+name = input('what is your name? ')
+print(f'Hello {name} and welcome to TalkPython[\'Training\']!')
