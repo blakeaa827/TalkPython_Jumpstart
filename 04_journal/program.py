b389ebe7ecaf44bf09d5050404533604b0eba3fe
Blake <blakeaa@gmail.com>
2018-03-25 20:28:59 -0500

    app 4 version 1 complete

--- 04_journal/program.py
+++ 04_journal/program.py
@@ -20,33 +20,46 @@ def load_journal(filename):
         journal = []
         with open(filename, 'r') as file:
             for line in file:
-                journal += [line]
+                journal += [line.rstrip()]
         print(f'... loaded {len(journal)} items')
         return journal
 
 
 def add_entry(new_entries):
-    return new_entries.append([input('Enter new journal entry below\n')])
+    return new_entries + [input('Enter new journal entry below\n')]
 
 
 def list_journal(journal, new_entries):
+    if not journal and not new_entries:
+        print('No entries to display')
+        return()
+    else:
+        pass
+
     line_number = 1
+
     if journal:
         for line in journal:
             print(f'{line_number}. {line}')
             line_number += 1
-    elif new_entries:
+    else:
+        pass
+
+    if new_entries:
         for line in new_entries:
             print(f'{line_number}. {line}')
             line_number += 1
     else:
-        print('No entries to display')
+        pass
+
+    return()
 
 
 def save_exit(filename, new_lines):
     print(f'... saving new journal entries to {filename} ...')
     with open(filename, 'a') as file:
-            file.writelines(new_lines)
+        for line in new_lines:
+            file.write(line + '\n')
     print('... save complete ...')
 
 
@@ -65,16 +78,16 @@ def get_command():
 def main():
     journal_file = './data/test_journal.jrn'
     journal = load_journal(journal_file)
-    new_lines = []
+    journal_adds = []
 
     while True:
         command = get_command()
         if command == 'list':
-            list_journal(journal, new_lines)
+            list_journal(journal, journal_adds)
         elif command == 'add':
-            new_lines = add_entry(new_lines)
+            journal_adds = add_entry(journal_adds)
         elif command == 'exit':
-            save_exit(journal_file, new_lines)
+            save_exit(journal_file, journal_adds)
             exit(0)
         else:
             print('That is not a valid command. Try again.')
