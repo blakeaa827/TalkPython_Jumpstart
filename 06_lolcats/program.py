518b3e2f320eeed956ff7ce333e60b9763ac9e80
Blake <blakeaa@gmail.com>
2018-03-27 21:27:19 -0500

    app 06 lolcats version 1 complete

--- 06_lolcats/program.py
+++ 06_lolcats/program.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import subprocess
 from bs4 import BeautifulSoup
 
 
@@ -17,20 +18,33 @@ def get_dir(name):
         os.mkdir(dir_path)
     else:
         print(f'Found {dir_path}.  Using existing location.')
+    return dir_path
 
 
-def get_lolcat():
+def get_lolcat(dir_path):
     r = requests.get('http://www.lolcats.com')
     soup: BeautifulSoup = BeautifulSoup(r.text, 'html.parser')
-    print(soup.find(class_='lolcat'))
+    for line in soup.findAll(class_='lolcat'):
+        img_url = 'http://www.lolcats.com' + line.get('src')
+        file_name = img_url.split('/')[-1]
+        img_response = requests.get(img_url)
+        img = img_response.content
+        file_path = os.path.join(dir_path, file_name)
+        with open(file_path, 'wb') as img_file:
+            img_file.write(img)
+
+
+def open_lolcat(dir_path):
+    subprocess.Popen(f'explorer "{dir_path}"')
 
 
 def main():
     dir_name = 'lolcats'
 
     print_header()
-    get_dir(dir_name)
-    get_lolcat()
+    dir_path = get_dir(dir_name)
+    get_lolcat(dir_path)
+    open_lolcat(dir_path)
 
 
 if __name__ == '__main__':
