6b08f586535d8d8ee3dd5982a0b980ae7157e92d
Blake <blakeaa@gmail.com>
2018-03-28 14:14:27 -0500

    app 06 lolcats version 2 using platform, shutil, and subprocess modules

--- 06_lolcats/program.py
+++ 06_lolcats/program.py
@@ -1,5 +1,7 @@
 import os
+import platform
 import requests
+import shutil
 import subprocess
 from bs4 import BeautifulSoup
 
@@ -12,7 +14,7 @@ def print_header():
 
 
 def get_dir(name):
-    dir_path = os.path.join('.', name)
+    dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), name))
     if not os.path.isdir(dir_path):
         print(f'Could not find {dir_path}.  Creating it now.')
         os.mkdir(dir_path)
@@ -22,20 +24,30 @@ def get_dir(name):
 
 
 def get_lolcat(dir_path):
-    r = requests.get('http://www.lolcats.com')
+    r = requests.get('http://www.lolcats.com/gallery/new.html')
     soup: BeautifulSoup = BeautifulSoup(r.text, 'html.parser')
-    for line in soup.findAll(class_='lolcat'):
-        img_url = 'http://www.lolcats.com' + line.get('src')
+    for line in soup.findAll(class_='gallery-item')[:10]:
+        img_page_url = 'http://www.lolcats.com' + line.a.get('href')
+        r = requests.get(img_page_url)
+        img_soup = BeautifulSoup(r.text, 'html.parser')
+        img_url = 'http://www.lolcats.com' + img_soup.find(class_='lolcat').get('src')
+        img = requests.get(img_url, stream=True).raw
         file_name = img_url.split('/')[-1]
-        img_response = requests.get(img_url)
-        img = img_response.content
         file_path = os.path.join(dir_path, file_name)
-        with open(file_path, 'wb') as img_file:
-            img_file.write(img)
+        with open(file_path, 'wb') as fout:
+            print(f'copying file {file_name}')
+            shutil.copyfileobj(img, fout)
 
 
 def open_lolcat(dir_path):
-    subprocess.Popen(f'explorer "{dir_path}"')
+    if platform.system() == 'Windows':
+        subprocess.call(['explorer', dir_path])
+    elif platform.system() == 'Linux':
+        subprocess.call(['xdg-open', dir_path])
+    elif platform.system() == 'Darwin':
+        subprocess.call(['open', dir_path])
+    else:
+        print(f'Sorry, we don\'t support your platform, {platform.system()}.')
 
 
 def main():
