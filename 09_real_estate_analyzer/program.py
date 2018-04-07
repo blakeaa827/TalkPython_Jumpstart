65a05146c5effb153b1496a3df34f92c0a6b64ad
Blake <blakeaa@gmail.com>
2018-04-06 22:11:59 -0500

    09 real estate app in progress

--- 09_real_estate_analyzer/program.py
+++ 09_real_estate_analyzer/program.py
@@ -1,21 +1,39 @@
 import csv
 import os
+import rerecord
+import statistics
 
 def main():
+    purchases: list = get_records(file_location())
+    print('{:,}'.format(get_avg_sale(purchases)))
+    print(get_min_sale(purchases))
+    print(get_max_sale(purchases))
+
+
+def file_location():
     file = 'SacramentoRealEstateTransactions2008.csv'
     file_abs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                  'data', file)
-    purchases = get_records(file_abs_path)
-
-    for p in purchases:
-        print(p)
+    return file_abs_path
 
 
-def get_records(archive):
-    with open(archive, 'r', encoding='utf-8') as fin:
-        readCSV = csv.DictReader(fin)
-        records = [p for p in readCSV]
-    return records
+def get_records(file_path):
+    with open(file_path, 'r', encoding='utf-8') as fin:
+        reader = csv.DictReader(fin)
+        records = [r for r in reader]
+        return records
+
+
+def get_avg_sale(purchases):
+    return int(statistics.mean([int(s['price']) for s in purchases]))
+
+
+def get_min_sale(purchases):
+    return sorted(purchases, key=lambda p: int(p['price']))[0]
+
+
+def get_max_sale(purchases):
+    return sorted(purchases, key=lambda p: int(p['price']))[-1]
 
 
 if __name__ == '__main__':
