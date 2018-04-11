3045054eea0d6ff258e21064db894362dbcb52b2
Blake <blakeaa@gmail.com>
2018-04-10 23:03:28 -0500

    app 10 movie search functionality complete. need error handling.

--- 10_movie_search/program.py
+++ 10_movie_search/program.py
@@ -2,10 +2,16 @@ from movie_svc import search_movies
 
 def main():
     print_banner()
-
-    movies = sorted(get_movies(), key=lambda m: -m.year)
-    for m in movies:
-        print(f'{m.year} -- {m.title}')
+    while True:
+        search_for = input('For what movies should I search? (or E[x]it) ')
+        if search_for == 'x':
+            print('Good Bye!!!')
+            exit(0)
+        else:
+            movies = sorted(get_movies(search_for), key=lambda m: -m.year)
+            for m in movies:
+                print(f'{m.year} -- {m.title}')
+            print()
 
 
 def print_banner():
@@ -15,13 +21,8 @@ def print_banner():
     print('\n')
 
 
-def get_movies():
-    search_for = input('For what movies should I search? (or E[x]it) ')
-    if search_for == 'x':
-        print('Good Bye!!!')
-        exit(0)
-    else:
-        return search_movies(search_for)
+def get_movies(search_for):
+    return search_movies(search_for)
 
 
 if __name__ == '__main__':
